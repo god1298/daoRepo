515b5207a8a42ad85098b601c136a89b787d74c9
huangli <god1298@qq.com>
2014-04-08 00:13:50 +0800

    TESTTEST

--- myDao/Student.java
+++ myDao/Student.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class Student implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-	private int Id;
+	private int Id;// 
 	private String Name;
 	private int age;
 	private int gender;
